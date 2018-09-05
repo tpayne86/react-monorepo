f7ddd00861ed88da562a30b10d6f0a1d5076facc
hannad rehman <hannadrehman@hannads-MacBook-Pro.local>
2018-09-05 17:38:21 +0530

    initial changes

---
+++ packages/constants/index.js
@@ -0,0 +1,3 @@
+module.exports.apiEndpoints = require('./api/api.endpoints');
+module.exports.app = require('./app/app.constants');
+module.exports.elements = require('./elements/element.constants');
