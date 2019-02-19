2d57e372768ed78ec8267a23e04822d034bd2f2e
hannad rehman <hannadrehman@hannads-MacBook-Pro.local>
2019-02-20 02:26:07 +0530

    added package-generator

---
+++ packages/package-generator/src/index.js
@@ -0,0 +1,3 @@
+const { createPackage } = require('./lib');
+
+createPackage();
--- packages/utilities/lib/http/index.js
+++ packages/utilities/lib/http/index.js
@@ -19,4 +19,3 @@ Object.defineProperty(exports, "httpPost", {
 });
 
 var _http = require("./http.requestmaker");
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9odHRwL2luZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0FBQUEiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBodHRwR2V0LCBodHRwUG9zdCB9IGZyb20gJy4vaHR0cC5yZXF1ZXN0bWFrZXInO1xuXG5leHBvcnQgeyBodHRwR2V0LCBodHRwUG9zdCB9O1xuIl19
