c487aa851e88c97c0fd13eb1538a44ca792d8078
hannad rehman <hannadrehman@hannads-MacBook-Pro.local>
2019-03-15 00:14:23 +0530

    test codeship build

--- packages/webpack-build/src/config/webpack.production.js
+++ packages/webpack-build/src/config/webpack.production.js
@@ -96,7 +96,7 @@ module.exports = (env) => {
           parallel: true,
           sourceMap: true,
         }),
-        new OptimizeCSSAssetsPlugin(),
+        new OptimizeCSSAssetsPlugin(), //
       ],
     },
   };
